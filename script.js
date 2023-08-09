66ec7a584e7d3a093eb6514f064a5ecb8cd53f3a
Aboubacar <minonaboubacars@gmail.com>
2023-08-09 17:06:15 +0000

    regrex en attente

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
 document.getElementById("inscription").addEventListener("submit", function(e) {
     
-    
     var erreur;
-    var nom = document.getElementById("nom");
-    var prenom = document.getElementById("prenom");
-    var email = document.getElementById("email");
-    var number = document.getElementById("number");
     
-    if (!nom.value) {
-        erreur = "Veuillez renseigner un nom";
-    }
+    var inputs =document.getElementsByTagName("input");
     
-    if (!prenom.value) {
-        erreur = "Veuillez renseigner un prénom";
-    }
-    
-    if (!email.value) {
-        erreur = "Veuillez renseigner un email";
-    }
-    
-    if (!number.value) {
-        erreur = "Veuillez renseigner un numero";
+    for (var i = 0; i < inputs.length; i++) {
+        if (!inputs[i].value) {
+            erreur = "Veuillez renseigner tous les champs";
+        }
     }
     
     if (erreur) {
-        e.preventDefault();
+         e.preventDefault();
         document.getElementById("erreur").innerHTML = erreur;
-        return false;
-    } else{
-        alert('Formulaire envoyé !');
-    }  
+            return false;
+         } else {
+           alert('Formulaire envoyé !');
+         }
+        }) 
+        
+        
+        
+        
+        
+        
+//     var nom = document.getElementById("nom");
+//     var prenom = document.getElementById("prenom");
+//     var email = document.getElementById("email");
+//     var number = document.getElementById("number");
+    
+//     if (!nom.value) {
+//         erreur = "Veuillez renseigner un nom";
+//     }
+    
+//     if (!prenom.value) {
+//         erreur = "Veuillez renseigner un prénom";
+//     }
+    
+//     if (!email.value) {
+//         erreur = "Veuillez renseigner un email";
+//     }
+    
+//     if (!number.value) {
+//         erreur = "Veuillez renseigner un numero";
+//     }
+    
+//     if (erreur) {
+//         e.preventDefault();
+//         document.getElementById("erreur").innerHTML = erreur;
+//         return false;
+//     } else{
+//         alert('Formulaire envoyé !');
+//     }  
     
     
-});
+// });
